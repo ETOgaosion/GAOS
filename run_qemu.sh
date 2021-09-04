ca1b38390ada464897e39e0e536aa55620a1ec4e
Zhang Mi <zhangmi@ict.ac.cn>
2021-09-04 12:43:56 +0000

    Add the scripts to run

---
+++ run_qemu.sh
@@ -0,0 +1,9 @@
+UBOOT_PATH=/u-boot/u-boot
+IMAGE_PATH=image
+
+if [ "$1" == "ci" ];
+then
+	/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel ${UBOOT_PATH} -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
+else
+	/home/stu/OSLab-RISC-V/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel ${UBOOT_PATH} -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
+fi
