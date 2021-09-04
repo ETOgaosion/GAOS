d2056bcede9f622958987e7e6e6e84a2f4e79005
Zhang Mi <zhangmi@ict.ac.cn>
2021-09-04 13:10:31 +0000

    Update run_qemu.sh and debug.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,9 +1,8 @@
-UBOOT_PATH=/u-boot/u-boot
 IMAGE_PATH=image
 
 if [ "$1" == "ci" ];
 then
-	/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel ${UBOOT_PATH} -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
+	/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel /u-boot/u-boot -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
 else
-	/home/stu/OSLab-RISC-V/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel ${UBOOT_PATH} -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
+	/home/stu/OSLab-RISC-V/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel /home/stu/OSLab-RISC-V/u-boot/u-boot -drive if=none,format=raw,id=image,file=${IMAGE_PATH} -device virtio-blk-device,drive=image
 fi
